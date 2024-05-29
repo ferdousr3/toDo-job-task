e90799776c09a54fd156900a6d78f3032a1034e4
MD.Ferdous <75322918+ferdousr3@users.noreply.github.com>
2024-05-28 20:15:31 -0700

    Update useToken.js
    op

--- src/hooks/useToken.js
+++ src/hooks/useToken.js
@@ -6,7 +6,7 @@ const useToken = (user) => {
     const email = user?.user?.email;
     const currentUser = { email: email };
     if (email) {
-      fetch(`https://todojobtask.herokuapp.com/user/${email}`, {
+      fetch(`https://to-do-job.vercel.app/user/${email}`, {
         method: "PUT",
         headers: {
           "content-type": "application/json",
