1c41583102289d264f9a1d8c4730bd744a6f6d61
MD.Ferdous <75322918+ferdousr3@users.noreply.github.com>
2024-05-28 21:59:19 -0700

    Update Loading.js
    ioiii

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -3,7 +3,7 @@ import React from "react";
 const Loading = () => {
   return (
     <>
-      <div className="h-screen flex justify-center">
+      <div className="h-14 flex justify-center">
         <button
           disabled
           type="button"
